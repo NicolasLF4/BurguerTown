91e7a2a1e195fa722fa18be226dec952e3f276cd
NicolasLF4 <lafuentejoel2002@gmail.com>
2022-08-17 16:52:20 -0300

    NavBar y routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PanelConfigComponent } from './components/panel-config/panel-config.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { FuncionesComponent } from './components/funciones/funciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    PanelConfigComponent,
+    MenuComponent,
+    NavbarComponent,
+    UsuariosComponent,
+    FuncionesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/panel-config/panel-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-config',
+  templateUrl: './panel-config.component.html',
+  styleUrls: ['./panel-config.component.css']
+})
+export class PanelConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
